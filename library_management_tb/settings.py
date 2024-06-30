5c2291dc05bf09491d87ade83f8d1de40a3e7188
Ibrahimhowlader2 <sibrahim.ih2@gmail.com>
2024-06-30 07:18:19 +0600

    CSRF_TRUSTED_ORIGINS

--- library_management_tb/settings.py
+++ library_management_tb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+CSRF_TRUSTED_ORIGINS = ['https://library-management-ccsh.onrender.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
